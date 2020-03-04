bb54f37cfb259fc88c4b4003c3c65604eca4cf44
Dexlansoprazole <prtd.lambo@gmail.com>
2020-03-04 14:49:04 +0800

    chore: heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./models/Members');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/prcnhrms');
 
 app.use(bodyParser.json());
 
