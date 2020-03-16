f9c215df55d03b48f0f5da383d637d98932d53d0
Dexlansoprazole <prtd.lambo@gmail.com>
2020-03-16 20:55:29 +0800

    fix: redirect path "/" to "/home"

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,37 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, NavLink, Redirect} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import SignIn from './components/SignIn';
 import MemberManagement from './components/MemberManagement';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <BrowserRouter>
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="navbar-collapse collapse justify-content-between" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                  <NavLink className="nav-item nav-link" to="home">Home</NavLink>
-                  <NavLink className="nav-item nav-link" to="member_management">成員管理</NavLink>
-                </div>
-                  <SignIn></SignIn>
+const App = () => {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+              aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="navbar-collapse collapse justify-content-between" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <NavLink className="nav-item nav-link" to="home">Home</NavLink>
+                <NavLink className="nav-item nav-link" to="member_management">成員管理</NavLink>
               </div>
+                <SignIn></SignIn>
             </div>
-          </nav>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/home" component={Home} />
-            <Route path="/member_management" component={MemberManagement} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/member_management" component={MemberManagement} />
+          <Redirect to="/home" />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
