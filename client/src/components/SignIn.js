8c9e9267051e700484db4c4b77671462b798f667
Dexlansoprazole <prtd.lambo@gmail.com>
2020-03-18 21:28:55 +0800

    feat: add google icon

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import signInActions from '../actions/signIn';
+import {IconNormal, IconPressed} from './GoogleSignInIcon';
 
 const SignIn = () => {
   const dispatch = useDispatch();
@@ -9,16 +10,31 @@ const SignIn = () => {
   const initGapi = () => dispatch(signInActions.initGapi());
   const logout = () => dispatch(signInActions.logout());
 
+  const [isSignInHover, setisSignInHover] = useState(false);
+
   useEffect(() => {
     window.gapi.load('auth2', () => {
       initGapi();
     });
   }, []);
 
+  const signInStyle = {
+    paddingTop: '4px',
+    paddingRight: '11px',
+    paddingBottom: '4px',
+    paddingLeft: '4px',
+    border: "0",
+    fontFamily: "Roboto",
+    backgroundColor: isSignInHover ? "#EEEEEE" : "#FFFFFF"
+  }
+
   return (
     <div className="navbar-nav">
-      <div className={"nav-item align-items-center" + (isSignedIn ? " d-none" : " d-flex")} style={{width: "80px", height: "40px", padding: "4px"}}>
-        <button id="signIn" className="btn btn-sm btn-light text-dark text-center align-text-top w-100 h-100 d-inline">登入</button>
+      <div className={"nav-item align-items-center" + (isSignedIn ? " d-none" : " d-flex")}>
+        <button id="signIn" className="btn btn-sm text-dark align-items-center w-100 h-100 d-flex" style={signInStyle} onMouseEnter={() => setisSignInHover(true)} onMouseLeave={() => setisSignInHover(false)}>
+          {isSignInHover ? (<IconPressed></IconPressed>) : (<IconNormal></IconNormal>) }
+          <span style={{paddingLeft: "7px"}}>以 Google 登入</span>
+        </button>
       </div>
       <div className={"nav-item text-light text-center align-items-center" + (isSignedIn ? " d-flex" : " d-none")} style={{height: "40px", padding: "4px"}}>{"您好，" + user.email}</div>
       <div className={"nav-item align-items-center" + (isSignedIn ? " d-flex" : " d-none")} style={{width: "80px", height: "40px", padding: "4px"}}>
