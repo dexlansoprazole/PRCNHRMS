bad7c7c4659e337f15fc53163d5edc247b515e02
Dexlansoprazole <prtd.lambo@gmail.com>
2020-03-26 22:41:12 +0800

    feat: add slide down effect to team dropdown

--- client/src/components/team/TeamDropdown.js
+++ client/src/components/team/TeamDropdown.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 import TeamDropdownItem from './TeamDropdownItem';
 
@@ -6,9 +6,19 @@ const TeamDropdown = () => {
   const teams = useSelector(state => state.team.teams);
   const teamSelected = useSelector(state => state.team.teamSelected);
 
+  useEffect(() => {
+    window.$('.dropdown').on('show.bs.dropdown', function() {
+      window.$(this).find('.dropdown-menu').first().stop(true, true).slideDown('fast');
+    });
+
+    window.$('.dropdown').on('hide.bs.dropdown', function() {
+      window.$(this).find('.dropdown-menu').first().stop(true, true).slideUp('fast');
+    });
+  });
+
   return (
-    <div className="align-items-center d-flex">
-      <h1 className="dropdown-toggle text-dark align-items-center d-flex" data-toggle="dropdown">{teamSelected.name}</h1>
+    <div className="dropdown" id="teamDropdown">
+      <h1 className="dropdown-toggle text-dark" data-toggle="dropdown">{teamSelected.name}</h1>
       <div className="dropdown-menu">
         <h6 className="dropdown-header">戰隊選擇</h6>
         {
