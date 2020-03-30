986c57c20b3051b57ede12d0c83ab4b44a2ad900
dexlansoprazole <prtd.lambo@gmail.com>
2020-03-30 14:38:19 +0800

    feat: make tables responsive

--- client/src/components/member/MemberTable.js
+++ client/src/components/member/MemberTable.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import MemberTableItem from './MemberTableItem';
 
 const MemberTable = props => {
+  const tableRef = useRef(null);
+
+  useEffect(() => {
+    let $table = window.$(tableRef.current);
+    $table.css('max-height', parseInt(window.$(window).height()) - $table.offset().top - 50);
+    window.$(window).resize(() => $table.css('max-height', parseInt(window.$(window).height()) - $table.offset().top - 50));
+  }, []);
+
   return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">ID</th>
-          <th scope="col">暱稱</th>
-          <th scope="col">加入日期</th>
-          <th scope="col">退出日期</th>
-          <th scope="col">踢除原因</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {(props.members && props.members.length > 0) ? (
-          props.members.map((member, index) => <MemberTableItem key={index} member={member} index={index + 1}></MemberTableItem>)
-        ) : (
-          <tr><td colSpan="7">查無成員：3</td></tr>
-        )}
-      </tbody>
-    </table>
+    <div className="table-responsive" ref={tableRef}>
+      <table className="table table-hover">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">ID</th>
+            <th scope="col">暱稱</th>
+            <th scope="col">加入日期</th>
+            <th scope="col">退出日期</th>
+            <th scope="col">踢除原因</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {(props.members && props.members.length > 0) ? (
+            props.members.map((member, index) => <MemberTableItem key={index} member={member} index={index + 1}></MemberTableItem>)
+          ) : (
+            <tr><td colSpan="7">查無成員：3</td></tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- client/src/components/team/TeamTable.js
+++ client/src/components/team/TeamTable.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import TeamTableItem from './TeamTableItem';
 
 const TeamTable = props => {
+  const tableRef = useRef(null);
+
+  useEffect(() => {
+    let $table = window.$(tableRef.current);
+    $table.css('max-height', parseInt((window.$(window).height()) - $table.offset().top - 50) / 2);
+    window.$(window).resize(() => $table.css('max-height', parseInt((window.$(window).height()) - $table.offset().top - 50) / 2));
+  }, []);
+
   return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">名稱</th>
-          <th scope="col">隊長</th>
-          <th scope="col">成員數</th>
-          <th scope="col">職位</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {(props.teams && props.teams.length > 0) ? (
-          props.teams.map((team, index) => <TeamTableItem key={index} team={team} index={index + 1} setTeamClicked={props.setTeamClicked}></TeamTableItem>)
-        ) : (
-            <tr><td colSpan="4">您還沒有戰隊：3</td></tr>
-          )}
-      </tbody>
-    </table>
+    <div className="table-responsive" ref={tableRef}>
+      <table className="table table-hover">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">名稱</th>
+            <th scope="col">隊長</th>
+            <th scope="col">成員數</th>
+            <th scope="col">職位</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {(props.teams && props.teams.length > 0) ? (
+            props.teams.map((team, index) => <TeamTableItem key={index} team={team} index={index + 1} setTeamClicked={props.setTeamClicked}></TeamTableItem>)
+          ) : (
+              <tr><td colSpan="4">您還沒有戰隊：3</td></tr>
+            )}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
