baf9fbb900d4f4d1e0d5e84ba0225365fd82f68a
Dexlansoprazole <prtd.lambo@gmail.com>
2020-04-01 18:18:50 +0800

    fix: fix the issue of checkIsLeader on no user

--- utils/permission.js
+++ utils/permission.js
@@ -3,11 +3,13 @@ const Teams = mongoose.model('teams');
 const {PermissionError} = require('./error');
 
 const checkIsLeader = async (user, team_id) => {
-  const teams = await Teams.find({leader: user._id}).catch(err => {
-    Promise.reject(err);
-  });
-  if (teams.find(t => t._id.toString() === team_id))
-    return true;
+  if (user) {
+    const teams = await Teams.find({leader: user._id}).catch(err => {
+      Promise.reject(err);
+    });
+    if (teams.find(t => t._id.toString() === team_id))
+      return true;
+  }
   Promise.reject(new PermissionError());
 }
 
