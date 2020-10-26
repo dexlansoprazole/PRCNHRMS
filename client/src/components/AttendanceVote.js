d8e76bbc653b532285f7cc2e807e70328093c5ed
Dexlansoprazole <prtd.lambo@gmail.com>
2020-10-26 22:23:52 +0800

    fix: fix icon of vote button

--- client/src/components/AttendanceVote.js
+++ client/src/components/AttendanceVote.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import {useTheme} from '@material-ui/core/styles';
 import {useDispatch, useSelector} from 'react-redux';
 import {Grid, Checkbox} from '@material-ui/core';
-import {ThumbUpOutlined, ThumbDownOutlined} from '@material-ui/icons';
+import {ThumbUpOutlined, ThumbDownOutlined, ThumbUp, ThumbDown} from '@material-ui/icons';
 import memberActions from '../actions/member';
 import moment from 'moment';
 import "moment/locale/zh-tw";
 moment.locale("zh-tw");
 
 const BtnVote = (props) => {
-  const {Icon, voted, setVoted, onChange} = props;
+  const {Icon, CheckedIcon, voted, setVoted, onChange} = props;
   const theme = useTheme();
 
   return (
@@ -21,7 +21,7 @@ const BtnVote = (props) => {
       style={{padding: 5}}
       size='small'
       icon={<Icon htmlColor={theme.palette.text.secondary} />}
-      checkedIcon={<Icon htmlColor={theme.palette.primary.main} />}
+      checkedIcon={<CheckedIcon htmlColor={theme.palette.primary.main} />}
     >
     </Checkbox>
   );
@@ -45,6 +45,7 @@ const AttendanceVote = (props) => {
       <Grid>
         <BtnVote
           Icon={ThumbUpOutlined}
+          CheckedIcon={ThumbUp}
           voted={upvoted}
           setVoted={setUpvoted}
           onChange={(event) => {
@@ -72,6 +73,7 @@ const AttendanceVote = (props) => {
       <Grid>
         <BtnVote
           Icon={ThumbDownOutlined}
+          CheckedIcon={ThumbDown}
           voted={downvoted}
           setVoted={setDownvoted}
           onChange={(event) => {
